b7c44d61ec71822e64d5f07c1a60a4ea37e4f809
Hamed <hamedullah@gmail.com>
2021-11-26 11:31:08 +0200

    Initial Commit

---
+++ groupAndAZlist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  groupAndAZlist
+//
+//  Created by Hamed Amiry on 26.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
